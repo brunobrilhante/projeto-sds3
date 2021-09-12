88c7a0df63fca31d00bce18f36ca563f05a312e0
Bruno Brilhante <brunobrilhante53@gmail.com>
2021-09-12 18:01:21 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,19 +11,18 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    // FORMA ERRADA
-    let chartData : ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries };
-            console.log(chartData);
-        });
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
     /*
     const mockData = {
@@ -30,7 +30,7 @@ const DonutChart = () => {
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
     */
-    
+
     const options = {
         legend: {
             show: true
@@ -38,13 +38,12 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{ ...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
         />
-
     );
 }
 
